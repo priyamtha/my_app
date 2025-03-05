56fd340c5a841aad27cb424f449a2e80472f3505
priya <priyamtha1509@gmail.com>
2025-03-05 13:36:16 +0530

    :)

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Website</h1>
+      <h2>Name: priyamtha</h2>
+      <h3>Contact: priyamtha1509@gmail.com</h3>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home"; // Ensure this file exists
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
